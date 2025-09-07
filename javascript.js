399a6dd427ac52afb31dc4c23f1c2cacb302243e
Darion <166887207+dariont5@users.noreply.github.com.>
2025-09-07 17:21:53 -0500

    Feat: added resizing from button

--- javascript.js
+++ javascript.js
@@ -1,18 +1,29 @@
+// Initial Call
 const container = document.querySelector('.container');
 let row = 16;
 let col = 16;
+generateGrid()
 
-for (let i = 0; i < row; i++) {
+// Functions
+function generateGrid() {for (let i = 0; i < row; i++) {
     for (let j = 0; j < col; j++) {
         const squareDiv = document.createElement('div');
         squareDiv.className = 'square';
         container.appendChild(squareDiv);
-    }
-}
-
-const squares = document.querySelectorAll('.square');
-squares.forEach(square => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'black';
+        squareDiv.addEventListener('mouseover', () => {
+            squareDiv.style.backgroundColor = 'black';
     })
-});
+}}}
+
+const root = document.querySelector(':root');
+const containerSize = parseInt(getComputedStyle(root).getPropertyValue('--max-size'));
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    container.textContent = '';
+    const sideLength = +prompt('How many squares should the sides of the grid be?');
+    [row, col] = [sideLength, sideLength];
+    let boxSize = containerSize/sideLength;
+    boxSize = boxSize+'px';
+    root.style.setProperty('--box-size', boxSize);
+    generateGrid();
+})
